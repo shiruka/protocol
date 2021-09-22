0bb27b2b3e1655de2c253c28940f936da2ee2ef0
Hasan Demirtaş <utsukushihito@outlook.com>
2021-09-22 22:55:35 +0300

    minecraft packets.

---
+++ src/main/java/io/github/shiruka/protocol/MinecraftPacket.java
@@ -0,0 +1,10 @@
+package io.github.shiruka.protocol;
+
+import io.github.shiruka.network.Packet;
+
+/**
+ * an abstract class that represents Minecraft packets.
+ */
+public abstract class MinecraftPacket implements Packet {
+
+}
