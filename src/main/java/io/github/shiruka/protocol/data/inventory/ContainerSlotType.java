2c3e6f5d968f64bfc44838564209e38dc7d95ec7
Hasan Demirtaş <utsukushihito@outlook.com>
2021-12-22 23:45:08 +0300

    Update ContainerSlotType.java

--- src/main/java/io/github/shiruka/protocol/data/inventory/ContainerSlotType.java
+++ src/main/java/io/github/shiruka/protocol/data/inventory/ContainerSlotType.java
@@ -4,64 +4,244 @@ package io.github.shiruka.protocol.data.inventory;
  * an enum class that contains container slot types.
  */
 public enum ContainerSlotType {
+  /**
+   * the anvil input.
+   */
   ANVIL_INPUT,
+  /**
+   * the anvil material.
+   */
   ANVIL_MATERIAL,
+  /**
+   * the anvil result.
+   */
   ANVIL_RESULT,
+  /**
+   * the smithing table input.
+   */
   SMITHING_TABLE_INPUT,
+  /**
+   * the smithing table material.
+   */
   SMITHING_TABLE_MATERIAL,
+  /**
+   * the smithing table result.
+   */
   SMITHING_TABLE_RESULT,
+  /**
+   * the armor.
+   */
   ARMOR,
+  /**
+   * the container.
+   */
   CONTAINER,
+  /**
+   * the beacon payment.
+   */
   BEACON_PAYMENT,
+  /**
+   * the brewing input.
+   */
   BREWING_INPUT,
+  /**
+   * the brewing result.
+   */
   BREWING_RESULT,
+  /**
+   * the brewing fuel.
+   */
   BREWING_FUEL,
+  /**
+   * the hotbar and inventory.
+   */
   HOTBAR_AND_INVENTORY,
+  /**
+   * the crafting input.
+   */
   CRAFTING_INPUT,
+  /**
+   * the crafting output.
+   */
   CRAFTING_OUTPUT,
+  /**
+   * the recipe construction.
+   */
   RECIPE_CONSTRUCTION,
+  /**
+   * the recipe nature.
+   */
   RECIPE_NATURE,
+  /**
+   * the recipe items.
+   */
   RECIPE_ITEMS,
+  /**
+   * the recipe search.
+   */
   RECIPE_SEARCH,
+  /**
+   * the recipe search bar.
+   */
   RECIPE_SEARCH_BAR,
+  /**
+   * the recipe equipment.
+   */
   RECIPE_EQUIPMENT,
+  /**
+   * the enchanting input.
+   */
   ENCHANTING_INPUT,
+  /**
+   * the enchanting lapis.
+   */
   ENCHANTING_LAPIS,
+  /**
+   * the furnace fuel.
+   */
   FURNACE_FUEL,
+  /**
+   * the furnace ingredient.
+   */
   FURNACE_INGREDIENT,
+  /**
+   * the furnace output.
+   */
   FURNACE_OUTPUT,
+  /**
+   * the horse equip.
+   */
   HORSE_EQUIP,
+  /**
+   * the hotbar.
+   */
   HOTBAR,
+  /**
+   * the inventory.
+   */
   INVENTORY,
+  /**
+   * the shulker.
+   */
   SHULKER,
+  /**
+   * the trade ingredient.
+   */
   TRADE_INGREDIENT1,
+  /**
+   * the trade ingredient 2.
+   */
   TRADE_INGREDIENT2,
+  /**
+   * the trade result.
+   */
   TRADE_RESULT,
+  /**
+   * the offhand.
+   */
   OFFHAND,
+  /**
+   * the compcreate input.
+   */
   COMPCREATE_INPUT,
+  /**
+   * the compcreate output.
+   */
   COMPCREATE_OUTPUT,
+  /**
+   * the elemconstruct output.
+   */
   ELEMCONSTRUCT_OUTPUT,
+  /**
+   * the matreduce input.
+   */
   MATREDUCE_INPUT,
+  /**
+   * the matreduce output.
+   */
   MATREDUCE_OUTPUT,
+  /**
+   * the labtable input.
+   */
   LABTABLE_INPUT,
+  /**
+   * the loom input.
+   */
   LOOM_INPUT,
+  /**
+   * the loom dye.
+   */
   LOOM_DYE,
+  /**
+   * the loom material.
+   */
   LOOM_MATERIAL,
+  /**
+   * the loom result.
+   */
   LOOM_RESULT,
+  /**
+   * the blast furnace ingredient.
+   */
   BLAST_FURNACE_INGREDIENT,
+  /**
+   * the smoker ingredient.
+   */
   SMOKER_INGREDIENT,
+  /**
+   * the trade 2 ingredient 1.
+   */
   TRADE2_INGREDIENT1,
+  /**
+   * the trade 2 ingredient 2.
+   */
   TRADE2_INGREDIENT2,
+  /**
+   * the trade 2 result.
+   */
   TRADE2_RESULT,
+  /**
+   * the grindstone input.
+   */
   GRINDSTONE_INPUT,
+  /**
+   * the grindstone additional.
+   */
   GRINDSTONE_ADDITIONAL,
+  /**
+   * the grindstone result.
+   */
   GRINDSTONE_RESULT,
+  /**
+   * the stonecutter input.
+   */
   STONECUTTER_INPUT,
+  /**
+   * the stonecutter result.
+   */
   STONECUTTER_RESULT,
+  /**
+   * the cartography input.
+   */
   CARTOGRAPHY_INPUT,
+  /**
+   * the cartography additional.
+   */
   CARTOGRAPHY_ADDITIONAL,
+  /**
+   * the cartography result.
+   */
   CARTOGRAPHY_RESULT,
+  /**
+   * the barrel.
+   */
   BARREL,
+  /**
+   * the cursor.
+   */
   CURSOR,
+  /**
+   * the creative output.
+   */
   CREATIVE_OUTPUT
 }
