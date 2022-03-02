c9048c041c623e0a8f6ce181d5e1a64dda377805
Hasan Demirtaş <utsukushihito@outlook.com>
2022-03-02 17:28:19 +0300

    Create BlockEvenV291.java

---
+++ src/main/java/io/github/shiruka/protocol/codec/v291/encoders/BlockEvenV291.java
@@ -0,0 +1,32 @@
+package io.github.shiruka.protocol.codec.v291.encoders;
+
+import io.github.shiruka.network.PacketBuffer;
+import io.github.shiruka.protocol.MinecraftSession;
+import io.github.shiruka.protocol.codec.CodecHelper;
+import io.github.shiruka.protocol.codec.PacketEncoder;
+import io.github.shiruka.protocol.codec.PacketId;
+import io.github.shiruka.protocol.packets.BlockEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * a class that represents block event packet encoders.
+ */
+@PacketId(26)
+public final class BlockEvenV291 extends PacketEncoder.Base<BlockEvent> {
+
+  @Override
+  public void decode(@NotNull final BlockEvent packet, @NotNull final CodecHelper helper,
+                     @NotNull final PacketBuffer buffer, @NotNull final MinecraftSession session) {
+    packet.blockPosition(buffer.readBlockPosition());
+    packet.eventType(buffer.readVarInt());
+    packet.eventData(buffer.readVarInt());
+  }
+
+  @Override
+  public void encode(@NotNull final BlockEvent packet, @NotNull final CodecHelper helper,
+                     @NotNull final PacketBuffer buffer, @NotNull final MinecraftSession session) {
+    buffer.writeBlockPosition(packet.blockPosition());
+    buffer.writeVarInt(packet.eventType());
+    buffer.writeVarInt(packet.eventData());
+  }
+}
